d26fe55c3985634eb1103614ab3f88ee1af80cfe
Murilo Jatobá <murilojatoba@gmail.com>
2025-04-13 17:27:01 -0300

    Importação do projeto gerado pelo Spring Initializr.

---
+++ src/main/java/com/github/murilojatoba/unyleya_ecommerce_backend/UnyleyaEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.murilojatoba.unyleya_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnyleyaEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnyleyaEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/murilojatoba/unyleya_ecommerce_backend/UnyleyaEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.murilojatoba.unyleya_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnyleyaEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
